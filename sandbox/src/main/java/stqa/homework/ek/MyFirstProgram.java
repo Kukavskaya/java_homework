7062d0c56230095e714d03798966a002bdc76d47
Ekaterina <_kat_@mail.ru>
2020-12-03 00:08:55 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/stqa/homework/ek/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/homework/ek/MyFirstProgram.java
@@ -1,9 +1,7 @@
 package stqa.homework.ek;
 
 public class MyFirstProgram {
-
-	public static void main (String [] args) {
-	System.out.println("Let's Start!");
-
-	}
+  public static void main(String[] args) {
+    System.out.println("Let's Start!");
+  }
 }
