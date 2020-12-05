9aa96d3756bac987e0d0142f4b8841b2b12375d5
Ekaterina <_kat_@mail.ru>
2020-12-06 00:17:35 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/stqa/homework/ek/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/homework/ek/MyFirstProgram.java
@@ -1,7 +1,30 @@
 package stqa.homework.ek;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
+
   public static void main(String[] args) {
-    System.out.println("Let's Start!");
+    hello("user");
+    hello("Kat");
+
+    double len = 5;
+    System.out.println("Плащадь квадрата со стороной " + len + "=" + aria (len));
+
+    double a = 2;
+    double b = 4;
+    System.out.println("Плащадь прямоугольника со стороной " + a + " и " + b + " = " + aria (a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Let's Start, " + somebody +"!");
+  }
+
+  public static double aria (double l) {
+    return l * l;
+  }
+
+  public static double aria (double a, double b) {
+    return a*b;
   }
 }
