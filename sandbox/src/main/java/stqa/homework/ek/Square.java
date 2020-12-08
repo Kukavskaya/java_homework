2e6f8f099f4d93911c62a43138a382c22a5b0b86
Ekaterina <_kat_@mail.ru>
2020-12-09 00:27:22 +0300

    Выделен класс Square

---
+++ sandbox/src/main/java/stqa/homework/ek/Square.java
@@ -0,0 +1,14 @@
+package stqa.homework.ek;
+
+public class Square {
+
+  public double l;
+
+  public Square (double l) {
+    this.l=l;
+  }
+
+  public double aria () {
+    return this.l * this.l;
+  }
+}
