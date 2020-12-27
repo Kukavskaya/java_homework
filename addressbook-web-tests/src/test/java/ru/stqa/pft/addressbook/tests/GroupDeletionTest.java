42fa73c3d5f87921a9e75176594b8b5e1da85b1f
Ekaterina <_kat_@mail.ru>
2020-12-27 23:18:30 +0300

    Создание класса помощника GroupHelper
    1. Создание класса помощника GroupHelper и перенос всех методов, связанных с работой с группами
    2. Перенос ссылки на драйвер обратно в класс ApplicationManager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -7,9 +7,9 @@ public class GroupDeletionTest extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.gotoGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGorups();
-    app.returnToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGorups();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }
