bdc23ca331d5559b8c9d49c23617bb6d422eb3f6
Ekaterina <_kat_@mail.ru>
2020-12-27 22:37:17 +0300

    Замена наследования на делегирование
    1. Переносим все вспомогательные функции в класс Application Manager
    2. TestBase класс отвечает за запуск и остановку метода
    3.Создание 3х папок для разделение слоев физически.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 
@@ -6,10 +6,10 @@ public class GroupDeletionTest extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-    gotoGroupPage();
-    selectGroup();
-    deleteSelectedGorups();
-    returnToGroupPage();
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGorups();
+    app.returnToGroupPage();
   }
 
 }
