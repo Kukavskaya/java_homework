6b6deb4a879d57e61c95a6ea350b90b7b4884519
Ekaterina <_kat_@mail.ru>
2021-01-03 16:44:18 +0300

    Свёртка вложенных проверок

--- sandbox/src/main/java/stqa/homework/ek/Equation.java
+++ sandbox/src/main/java/stqa/homework/ek/Equation.java
@@ -8,27 +8,16 @@ public class Equation {
 
   private int n;
 
-  public Equation (double a, double b, double c) {
+  public Equation(double a, double b, double c) {
 
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n= 0;
-        }
-      } else {
-        n = 1;
-      }
-
-    } else {
-      if (d>0) {
+    if (a != 0) {
+      if (d > 0) {
         n = 2;
       } else if (d == 0) {
         n = 1;
@@ -36,6 +25,14 @@ public class Equation {
         n = 0;
       }
 
+    } else if (b != 0) {
+      n = 1;
+
+    } else if ( c != 0 ) {
+      n = 0;
+
+    } else {
+      n = -1;
     }
 
   }
