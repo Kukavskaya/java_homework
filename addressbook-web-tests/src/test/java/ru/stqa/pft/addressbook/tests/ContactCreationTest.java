700a138db0760f44247d5c23aef676d6e5ec8904
Ekaterina <_kat_@mail.ru>
2021-01-14 20:39:58 +0300

    Формирование коллекиции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -7,7 +8,10 @@ public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().createContact(new ContactData("user1", "user2", "address1", "+79999999999", "testuser@mail.ru", "test1"), true);
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(before, before + 1);
     app.Logout();
   }
 }
