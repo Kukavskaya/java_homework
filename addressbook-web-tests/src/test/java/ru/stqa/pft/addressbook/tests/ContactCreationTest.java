f76dc7b94f07f285ea08faaee27098378d117d8d
Ekaterina <_kat_@mail.ru>
2021-02-01 01:57:56 +0300

    Реализация провайдеров тестовых данных, которые читают данные из файлов, для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,31 +1,54 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contact.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     int index = before.size() + 1;
-    File photo = new File("src/test/resources/image.png");
-    ContactData contact = new ContactData().withFirstname("user1").withLastname("user2").withAddress("address1")
-            .withTelephone("+79999999999").withEmail("testuser@mail.ru").withPhoto(photo);
+//    File photo = new File("src/test/resources/image.png");
+//    ContactData contact = new ContactData().withFirstname("user1").withLastname("user2").withAddress("address1")
+//            .withTelephone("+79999999999").withEmail("testuser@mail.ru").withPhoto(photo);
     app.contact().create(contact, true);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(index));
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
-    app.Logout();
+//    app.Logout();
   }
 
   @Test (enabled = false)
@@ -39,7 +62,7 @@ public class ContactCreationTest extends TestBase {
 //    Assert.assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before));
 
-    app.Logout();
+//    app.Logout();
   }
 
   @Test (enabled = false)
