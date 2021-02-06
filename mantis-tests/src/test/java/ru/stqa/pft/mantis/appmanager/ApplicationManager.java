50ffffedfd3b6afb4e906730f8a4e54b5774d5fa
Ekaterina <_kat_@mail.ru>
2021-02-06 19:25:02 +0300

    Добавлен новый модуль для тестов для Mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  private final Properties properties;
+  protected WebDriver wd;
+
+
+  public boolean acceptNextAlert;
+  private String browser;
+
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.setProperty("target", "local");
+    properties.load(new FileReader(new File (String.format("src/test/resources/local.properties", target))));
+
+    if ( browser.equals(BrowserType.CHROME) ) {
+      wd = new ChromeDriver();
+    } else if ( browser.equals(BrowserType.FIREFOX) ) {
+      wd = new FirefoxDriver();
+    } else if ( browser.equals(BrowserType.IE) ) {
+      wd = new InternetExplorerDriver();
+    }
+
+    System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+    System.setProperty("webdriver.firefox.driver", "lib/geckodriver.exe");
+
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+
+}
