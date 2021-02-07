e1e136bd43e6a9e9877bdeb6f16f3edc3b3529fd
Ekaterina <_kat_@mail.ru>
2021-02-07 17:15:50 +0300

    Передача файлов на удаленную машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -16,8 +16,9 @@ public class ApplicationManager {
   private final Properties properties;
   private WebDriver wd;
 
-   private String browser;
+  private String browser;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
 
   public ApplicationManager(String browser) {
@@ -49,6 +50,13 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
+  }
+
+  public FtpHelper ftp (){
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
   }
 
   public WebDriver getDriver() {
